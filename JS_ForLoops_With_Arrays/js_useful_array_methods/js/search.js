e0e9e454b07d52e3a707a4c7da5729bde64f2681
Frank Kalmbach <frank.kalmbach@hotmail.com>
2015-03-19 19:16:24 -0400

    Ignoring the .DS_Store

--- JS_ForLoops_With_Arrays/js_useful_array_methods/js/search.js
+++ JS_ForLoops_With_Arrays/js_useful_array_methods/js/search.js
@@ -1,3 +1,9 @@
+/* JS to search "store" to return
+  (1) see if item is in stock (both in or not)
+  (2) print entire stock list if "list" input
+  (3) quit the input with "quit"
+*/
+
 
 
 var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
@@ -7,7 +13,16 @@ function print(message) {
   document.write( '<p>' + message + '</p>');
 }
 
-prompt(
+do {
+  search = prompt("What item are you looking for? Enter item name, 'list' for a full list of available items, or 'quit' to end the search.");
+  var itemSearch = inStock.indexOf(search.toLowerCase() );
+  if (itemSearch >= 0) {
+    print( search + ' is in stock.')'
+  }
+    
+  }
+
+} while (search.toLowerCase() !== 'quit');
 
 
 
