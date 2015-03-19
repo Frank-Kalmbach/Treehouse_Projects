0764e4309bdc00001fa2c1661c6ef945d01c682e
Frank Kalmbach <frank.kalmbach@hotmail.com>
2015-03-19 18:20:12 -0400

    Updating video 6 files

--- JS_ForLoops_With_Arrays/js_useful_array_methods/js/search.js
+++ JS_ForLoops_With_Arrays/js_useful_array_methods/js/search.js
@@ -1,3 +1,5 @@
+
+
 var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
 var search;
 
@@ -5,3 +7,31 @@ function print(message) {
   document.write( '<p>' + message + '</p>');
 }
 
+prompt(
+
+
+
+
+
+/*  Practice .join
+
+var daysInWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var daysString = daysInWeek.join(' - ');
+document.write(daysString);
+*/
+
+/* Practice .concat
+
+var currentStudents = [ 'Joan', 'John', 'Joauin'];
+var newStudents = [ 'Sam', 'Traci', 'Tiago'];
+var allStudents = currentStudents.concat( newStudents );
+document.write(currentStudents);
+document.write(newStudents);
+document.write(allStudents);
+*/
+
+/* Practice .indexOf
+var fruit = [ 'Apple', 'Orange', 'Pear'];
+var position = fruit.indexOf('Orange');
+alert(position);
+*/
