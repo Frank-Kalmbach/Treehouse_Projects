688785f88b196424ddf938243a7299f78f9e74e1
Frank Kalmbach <frank.kalmbach@hotmail.com>
2015-03-24 11:47:48 -0400

    Treehouse solution for Video 6

--- JS_ForLoops_With_Arrays/js_useful_array_methods/js/search.js
+++ JS_ForLoops_With_Arrays/js_useful_array_methods/js/search.js
@@ -4,7 +4,32 @@
   (3) quit the input with "quit"
 */
 
+/* Treehouse Solution */
 
+var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+var search;
+
+function print(message) {
+  document.write( '<p>' + message + '</p>');
+}
+
+while (true) {
+  search = prompt("Search for a product in our store. Type 'list' to show all of the products and 'quit' to exit");
+  search = search.toLowerCase();
+  if (search === 'quit') {
+    break;
+  } else if (search === 'list') {
+    print(inStock.join(', '));
+  } else {
+    if ( inStock.indexOf(search) > -1 ){
+      print('Yes, we have ' + search + ' in the store.');
+    } else {
+      print( search + ' is not in stock.');
+    }
+  }
+}
+
+/* Initial attempt prior to treehouse solution - do ... while created issues with quit showing not in stock before exiting program
 
 var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
 var search;
@@ -15,16 +40,23 @@ function print(message) {
 
 do {
   search = prompt("What item are you looking for? Enter item name, 'list' for a full list of available items, or 'quit' to end the search.");
-  var itemSearch = inStock.indexOf(search.toLowerCase() );
-  if (itemSearch >= 0) {
-    print( search + ' is in stock.')'
-  }
+
+  if (search.toLowerCase === 'quit') {
+    break;
+  
+  } else if (search.toLowerCase === 'list') {
+    print(inStock.join(', '));
     
+  } else if (itemSearch >= 0 ) {
+    print( search + ' is in stock.');
+
+  } else (itemSearch < 0) {
+    print( search + ' is not in stock.');
   }
 
 } while (search.toLowerCase() !== 'quit');
 
-
+*/
 
 
 
