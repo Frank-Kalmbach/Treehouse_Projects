b001e05406a5c9afb4512ff759c57c4e59d7ddc5
Frank Kalmbach <frank.kalmbach@hotmail.com>
2015-03-24 13:36:36 -0400

    Video 7 completion

--- JS_ForLoops_With_Arrays/vid7_js_twodimensional_arrays/js/playlist.js
+++ JS_ForLoops_With_Arrays/vid7_js_twodimensional_arrays/js/playlist.js
@@ -1,34 +1,43 @@
+/* using 2-dim array to print artists and songs */
+
 var playList = [
-  'I Did It My Way',
-  'Respect',
-  'Imagine',
-  'Born to Run',
-  'Louie Louie',
-  'Maybellene'
+  ['I Did It My Way', 'Frank Sinatra'],
+  ['Respect', 'Aretha Franklin'],
+  ['Imagine', 'John Lennon'],
+  ['Born to Run', 'Bruce Springsteen'],
+  ['Louie Louie', 'The Kingsmen'],
+  ['Maybellene', 'Chuck Berry']
 ];
+
+
 
 function print(message) {
   document.write(message);
 }
 
-function printList( list ) {
+function printSongs( songs ) {
   var listHTML = '<ol>';
-  for ( var i = 0; i < list.length; i += 1) {
-    listHTML += '<li>' + list[i] + '</li>';
+  for ( var i = 0; i < songs.length; i += 1) {
+    listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1]+ '</li>';
   }
   listHTML += '</ol>';
   print(listHTML);
 }
 
-printList(playList);
+printSongs(playList);
 
 
+/* Video 7 example
 
+var grades = [
+  [80, 90, 100, 95],
+  [75, 95, 85, 100],
+  [60, 80, 77, 90]
+]
 
+alert(grades[1][2]);
 
-
-
-
+*/
 
 
 
