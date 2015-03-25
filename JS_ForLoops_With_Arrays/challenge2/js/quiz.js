69cee7242a5a917db5b6e9c14d21dc33830bde1c
Frank Kalmbach <frank.kalmbach@hotmail.com>
2015-03-25 00:22:02 -0400

    JS Challenge2 Answer

--- JS_ForLoops_With_Arrays/challenge2/js/quiz.js
+++ JS_ForLoops_With_Arrays/challenge2/js/quiz.js
@@ -1,3 +1,12 @@
+/* Improve on challenge1 quiz by displaying a list of questions the player answered correctly and a list of questions the player got wrong. 
+"You got 2 question(s) right."
+"You got these questions correct:
+    1. How many states are in the United States?
+    2. How many legs does an insect have?
+"You got these questions wrong:
+    1. How many continents are there?"
+*/
+
 var questions = [
   ['How many states are in the United States?', 50],
   ['How many continents are there?', 7],
@@ -7,9 +16,21 @@ var correctAnswers = 0;
 var question;
 var answer;
 var response;
+var correct = [];
+var wrong = [];
 
 function print(message) {
-  document.write(message);
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+  var listHTML = '<ol>';
+    for (var i = 0; i < arr.length; i += 1) {
+      listHTML += '<li>' +arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML;
 }
 
 for (var i = 0; i < questions.length; i += 1) {
@@ -19,8 +40,16 @@ for (var i = 0; i < questions.length; i += 1) {
   response = parseInt(response);
   if (response === answer) {
     correctAnswers += 1;
+    correct.push(question);
+  }else{
+    wrong.push(question);
   } 
 }
 
 html = "You got " + correctAnswers + " question(s) right."
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong);
 print(html);
+
